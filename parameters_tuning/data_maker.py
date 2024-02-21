f82991aa20212ae73ccb556862446f20538ffe1e
Fenyutanchan WU <fenyutanchan@vip.qq.com>
2024-02-22 00:30:27 +0800

    finally tuning.

--- parameters_tuning/data_maker.py
+++ parameters_tuning/data_maker.py
@@ -19,20 +19,43 @@ flux_unit = sapf.flux_unit(1e10, unit)
 energy_length = 1000
 mass_length = 5
 
-photon_energy_list = np.linspace(.1*keV, 19*keV, energy_length)
+# photon_energy_list = np.linspace(.1*keV, 19*keV, energy_length)
 axion_mass_list = np.linspace(0, 4*keV, mass_length)
 # dΦ_dE_list = [[sapf.solar_axion_flux(E, solar_data, unit=unit, g_aγ=g10, axion_mass=ma) for E in photon_energy_list] for ma in axion_mass_list]
 
-data = np.zeros((energy_length * mass_length, 3))
+# data = np.zeros((energy_length * mass_length, 3))
 
-data_index = 0
-for ma in axion_mass_list:
-    for E in photon_energy_list:
-        data[data_index, 0] = E / keV
-        data[data_index, 1] = ma / keV
-        data[data_index, 2] = sapf.solar_axion_flux(E, solar_data, unit=unit, g_aγ=g10, axion_mass=ma) / flux_unit
-        data_index += 1
-    # end for E
+normalized_factor = [7.2, 6.6, 4.8, 2.9, 1.5]
+
+photon_energy_list = []
+ma_list = []
+dΦ_dE_list = []
+# data_index = 0
+for (ii, ma) in enumerate(axion_mass_list):
+    E_over_keV = .1
+    step_base = .1 * normalized_factor[ii] / 10
+    while E_over_keV < 19:
+        E = E_over_keV * keV
+        flux = sapf.solar_axion_flux(E, solar_data, unit=unit, g_aγ=g10, axion_mass=ma) / flux_unit
+        if flux == 0:
+            E_over_keV += step_base
+            continue
+
+        photon_energy_list.append(E_over_keV)
+        ma_list.append(ma / keV)
+        dΦ_dE_list.append(flux)
+
+        E_over_keV += step_base / (10 * flux)
+    # end while
+
+    # for E in photon_energy_list:
+    #     data[data_index, 0] = E / keV
+    #     data[data_index, 1] = ma / keV
+    #     data[data_index, 2] = sapf.solar_axion_flux(E, solar_data, unit=unit, g_aγ=g10, axion_mass=ma) / flux_unit
+    #     data_index += 1
+    # # end for E
 # end for ma
+
+data = np.array([photon_energy_list, ma_list, dΦ_dE_list]).T
 
 np.savetxt('solar_axion_flux_data.csv', data, delimiter=',', header="photon_energy,axion_mass,flux", comments="")
